87f3f0e317800a68e3f59c8eccd741c1f2d3297e
Frans Trujillo F <frans.trujillo@protonmail.com>
2023-07-06 00:15:16 -0500

    Implementacion de Tabu Search completo

--- codeSolucionInicial.jl
+++ codeSolucionInicial.jl
@@ -18,7 +18,7 @@ cant_especialistas=length(Especialistas)
 cant_proyectos=length(diccionarios)
 iteraciones=parse(Int,ARGS[1])
 cantidad_de_vecinos=5
-size_of_tabu=8
+size_of_tabu=6
 lista_tabu=[]
 #tabu_count=0
 #-----FIN DE LA DEFINICION DE VARIABLES GLOBALES-----
@@ -92,13 +92,7 @@ function retraso_especialista(lista)
     end    
 end    
 
-#Funcion que escoje a los especialista con mayor y menor retraso, luego aleatoriamente hace
-#un swap entre proyectos, si esta nueva asignacion tiene menor retraso, la nueva asignacion procede. 
-function is_tabu()
-
-end    
-
-#ESTA FUNCION SÍ O SÍ HACE UN MOVIMIENTO.
+#Esta función coge un proyecto de un especialista y se lo asigno a otro
 function simple_move(lista_asignaciones_raw)
     #Escojo aleatoriamente un elemento como tal de mi lista_asignaciones
     lista_asignaciones=deepcopy(lista_asignaciones_raw)
@@ -106,7 +100,10 @@ function simple_move(lista_asignaciones_raw)
     #Escojo aleatoriamente a un especialista
     n_especialista_out=rand(1:cant_especialistas)
     n_especialista_in=rand(1:cant_especialistas)
-
+    while n_especialista_in==n_especialista_out
+        n_especialista_out=rand(1:cant_especialistas)
+        n_especialista_in=rand(1:cant_especialistas)
+    end     
     A=length(lista_asignaciones[n_especialista_out])
     B=length(lista_asignaciones[n_especialista_in])
     while A==0
@@ -127,7 +124,8 @@ function simple_move(lista_asignaciones_raw)
     return lista_asignaciones,(lista_asignaciones[n_especialista_in][posicion_in][1], n_especialista_in)
 end
 
-
+#Agarra al especialista con menor retraso y a otro con mayor retraso, y toma de ellos un proyecto aleatorio y los intercambia.
+#Esta funcion solo hace cambios en la lista que le pasas si encuentra una mejora, si no retorna la misma lista
 function asignacion_random(lista_de_proyectos_raw)
     global tabu_count
     lista_de_proyectos=deepcopy(lista_de_proyectos_raw)
@@ -137,7 +135,6 @@ function asignacion_random(lista_de_proyectos_raw)
     end
     indice_del_tardon=argmax(lista_de_retrasos)  
     indice_del_puntual=argmin(lista_de_retrasos)  
-    #data_tabu=[lista_de_proyectos_raw,indice_del_puntual,indice_del_tardon]
 
     cant_proye_tardon=length(lista_de_proyectos[indice_del_tardon])
     cant_proye_puntual=length(lista_de_proyectos[indice_del_puntual])
@@ -151,18 +148,13 @@ function asignacion_random(lista_de_proyectos_raw)
     tiempo_correcto(lista_de_proyectos[indice_del_puntual],indice_del_puntual)
     tiempo_correcto(lista_de_proyectos[indice_del_tardon],indice_del_tardon)
     sort_x_fem(lista_de_proyectos)
-    # if lista_de_proyectos in LISTA_TABU
-    #     tabu_count+=1
-    #     return lista_de_proyectos_raw
-    # end    
+  
     if retraso_general(lista_de_proyectos) < sum(lista_de_retrasos)
         temporal=lista_de_proyectos_raw[indice_del_tardon][indice_rand_tardon]
         lista_de_proyectos_raw[indice_del_tardon][indice_rand_tardon]=lista_de_proyectos_raw[indice_del_puntual][indice_rand_puntual]
         lista_de_proyectos_raw[indice_del_puntual][indice_rand_puntual]=temporal
         sort_x_fem(lista_de_proyectos_raw)
         return lista_de_proyectos_raw
-    else
-        #push!(LISTA_TABU,lista_de_proyectos)
     end
 
     return lista_de_proyectos_raw
@@ -183,9 +175,8 @@ function sort_x_fem(lista_de_proyectos)
     return lista_de_proyectos
 end
 
-#funcion que retorna la primera asignacion
-#esta solucion va asignando de acuerdo a la cantidad de tiempo transcurrido. 
 
+#esta solucion va asignando proyectos al especialista que tiene menor carga de trabajo
 function primera_solucion(asig_proyect,processing_time,FEM)
     #Buena forma de hacerlo
     #Solo tenemos que reorganizarlo, las fechas de procesamiento en el orden de las fechas máximas de entrega. 
@@ -202,12 +193,11 @@ function primera_solucion(asig_proyect,processing_time,FEM)
     return asig_proyect
 end 
 
-#Esta nueva solucion genera una mejora de 24 días, 681 horas, pasa de 7675 a 6994
+#esta solucion va asignando proyectos al especialista que tiene menor carga de trabajo y los proyectos que
+#se van asignando van en orden de su fecha máxima de entrega
 function primera_solucion2(asig_proyect,processing_time,FEM)
     FEM_order=deepcopy(FEM)
-    processing_time_order=deepcopy(processing_time)
     FEM_order=sort(FEM, by = x -> x[2])  
-    #println(FEM_order)
     
     for m in 1:cant_proyectos
         especialista_mas_nuevo_proyecto=[]
@@ -231,7 +221,7 @@ function primera_solucion2(asig_proyect,processing_time,FEM)
     return asig_proyect
 end    
 
-#Funcion que luego del swap de proyectos aleatorios, pone los tiempos de procesamiento adecuados a cada asignacion nueva. 
+#Funcion pone los tiempos de procesamiento adecuados a cada asignacion nueva.
 function tiempo_correcto(proyectos_x_especialista,numero_del_espe)
     for proyecto in proyectos_x_especialista
         numero_del_proye = parse(Int,match(r"\d+", proyecto[1]).match)
@@ -240,51 +230,19 @@ function tiempo_correcto(proyectos_x_especialista,numero_del_espe)
     end
 end
 
-# function primera_solucion3(asig_proyect,processing_time,FEM)
-#     listax=[]
-#     push!(listax,FEM[un_proyecto][2]-tiempo_total_especialista()) 
-#     indice_del_mejor especialista=argmax(listax)
-#     #la FEM que use tiene que estar en orden.
-
-#     FEM_order=deepcopy(FEM)
-#     processing_time_order=deepcopy(processing_time)
-#     FEM_order=sort(FEM, by = x -> x[2])  
-#     #println(FEM_order)
-
-#     for m in 1:cant_proyectos
-#         especialista_mas_nuevo_proyecto=[]
-#         #este for mete a la lista anterior, los tiempos de procesamiento que lleva cada especialista
-#         #se escoje al que menos tiempo tenga y se le asigna ese proyecto
-#         numero_del_proye = parse(Int,match(r"\d+",FEM_order[m][1]).match)
-#         for i in 1:20
-#             push!(especialista_mas_nuevo_proyecto,tiempo_total_especialista(asig_proyect[i])+processing_time[i][numero_del_proye])
-#         end
-
-#         indice_del_espe_asig=argmin(especialista_mas_nuevo_proyecto)
-
-
-#         push!(asig_proyect[indice_del_espe_asig],[FEM_order[m][1],FEM_order[m][2],processing_time[indice_del_espe_asig][m]])
-        
-#     end
-
-#     for i in 1:20
-#         tiempo_correcto(asig_proyect[i],i) 
-#     end    
-#     return asig_proyect
-
-# end
 #-----FIN DE LA DEFINICION DE LAS FUNCIONES-----
 
 
 #-----EJECUCION DE FUNCIONES
 
+#Solución inicial
 primera_solucion2(asignacion_de_proyectos,Tiempo_procesamiento,Proyectos_fecha_max)
 
 solucion_actual=deepcopy(sort_x_fem(asignacion_de_proyectos))
 println(retraso_general(solucion_actual))
+#Fin de la solucion inicial
 
-rango=collect(1:iteraciones)
-
+#TABU SEARCH
 for i in 1:iteraciones
     global cantidad_de_vecinos
     global solucion_actual
@@ -292,14 +250,11 @@ for i in 1:iteraciones
     lista_de_vecinos=[]
     retrasos_x_vecinos=[]
     candidatos_a_entrar_en_tabu=[]
-    if length(lista_tabu)==5
-        #Boto al primer elemento de la lista
+    if length(lista_tabu)==size_of_tabu
         popfirst!(lista_tabu)
     end    
     for i in 1:cantidad_de_vecinos
-        #println("ANTES DE MOVE")
         lista,(proyecto,especialista)=simple_move(solucion_actual)
-        #println("PASE MOVE")
         if (proyecto,especialista) in lista_tabu 
             continue
         else 
@@ -311,19 +266,26 @@ for i in 1:iteraciones
 
     indice_menor_retraso=argmin(retrasos_x_vecinos)
     push!(lista_tabu,candidatos_a_entrar_en_tabu[indice_menor_retraso])
-    #aquí asigno el mejor vecino como mi solucion actual
     solucion_actual=lista_de_vecinos[indice_menor_retraso]
     
-    #Aquí mi solucion global se actualizá si...
     if retrasos_x_vecinos[indice_menor_retraso] < retraso_general(asignacion_de_proyectos)
-        asignacion_de_proyectos=solucion_actual
-        println("NUEVO TIEMPO: ",retraso_general(asignacion_de_proyectos))
+        asignacion_de_proyectos=deepcopy(solucion_actual)
     end
+
+    #Periodo de intensificacion
+    if i%100==0
+        for i in 1:100
+            asignacion_random(asignacion_de_proyectos)
+        end  
+    end
+    #fin del periodo de intensificación
+
+    solucion_actual=deepcopy(asignacion_de_proyectos)
 end
 
-# for i in 1:4375
-#     simple_move(asignacion_de_proyectos)
-# end
 println("Retraso de $cant_proyectos proyectos con $iteraciones iteraciones: ",retraso_general(asignacion_de_proyectos))
-#println("Cantidad de soluciones que omito por estar en la lista tabu $tabu_count")
 writedlm("resultados.csv", asignacion_de_proyectos, ',')
+#EL ARCHIVO QUE CONTIENE EL RESULTADO DE LA SOLUCION SE LLAMA, resultados.csv 
+#PAGAR EJECUTAR ESTE ARCHIVO TIENES QUE EJECUTARLO EN LA TERMINAL CON EL SIGUIENTE COMANDO
+# julia codeSolucionInicial.jl 100000
+#100000 es la cantidad de iteraciones, si deseas más o menos cambialo. 
